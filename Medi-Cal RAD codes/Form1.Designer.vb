856d84054599e2ebde160c64128268f37510d7f3
Kory <KHanagan3193@gmail.com>
2022-10-20 16:19:43 -0700

    Initial commit

---
+++ Medi-Cal RAD codes/Form1.Designer.vb
@@ -0,0 +1,155 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.txtSearch = New System.Windows.Forms.TextBox()
+        Me.btnSearch = New System.Windows.Forms.Button()
+        Me.radRADs = New System.Windows.Forms.RadioButton()
+        Me.txtOutput = New System.Windows.Forms.RichTextBox()
+        Me.txtBilling = New System.Windows.Forms.RichTextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.radDate = New System.Windows.Forms.RadioButton()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'txtSearch
+        '
+        Me.txtSearch.Location = New System.Drawing.Point(97, 37)
+        Me.txtSearch.Name = "txtSearch"
+        Me.txtSearch.Size = New System.Drawing.Size(100, 20)
+        Me.txtSearch.TabIndex = 0
+        '
+        'btnSearch
+        '
+        Me.btnSearch.Location = New System.Drawing.Point(16, 35)
+        Me.btnSearch.Name = "btnSearch"
+        Me.btnSearch.Size = New System.Drawing.Size(75, 23)
+        Me.btnSearch.TabIndex = 1
+        Me.btnSearch.Text = "Search"
+        Me.btnSearch.UseVisualStyleBackColor = True
+        '
+        'radRADs
+        '
+        Me.radRADs.AutoSize = True
+        Me.radRADs.Location = New System.Drawing.Point(16, 12)
+        Me.radRADs.Name = "radRADs"
+        Me.radRADs.Size = New System.Drawing.Size(53, 17)
+        Me.radRADs.TabIndex = 2
+        Me.radRADs.TabStop = True
+        Me.radRADs.Text = "RADs"
+        Me.radRADs.UseVisualStyleBackColor = True
+        '
+        'txtOutput
+        '
+        Me.txtOutput.Location = New System.Drawing.Point(12, 78)
+        Me.txtOutput.Name = "txtOutput"
+        Me.txtOutput.ReadOnly = True
+        Me.txtOutput.Size = New System.Drawing.Size(395, 48)
+        Me.txtOutput.TabIndex = 4
+        Me.txtOutput.Text = ""
+        '
+        'txtBilling
+        '
+        Me.txtBilling.Location = New System.Drawing.Point(12, 143)
+        Me.txtBilling.Name = "txtBilling"
+        Me.txtBilling.ReadOnly = True
+        Me.txtBilling.Size = New System.Drawing.Size(395, 103)
+        Me.txtBilling.TabIndex = 5
+        Me.txtBilling.Text = ""
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 62)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Output"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 127)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(57, 13)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Billing Tips"
+        '
+        'radDate
+        '
+        Me.radDate.AutoSize = True
+        Me.radDate.Location = New System.Drawing.Point(97, 12)
+        Me.radDate.Name = "radDate"
+        Me.radDate.Size = New System.Drawing.Size(78, 17)
+        Me.radDate.TabIndex = 8
+        Me.radDate.TabStop = True
+        Me.radDate.Text = "Julian Date"
+        Me.radDate.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Medi_Cal_RAD_codes.My.Resources.Resources.Untitled
+        Me.PictureBox1.InitialImage = Nothing
+        Me.PictureBox1.Location = New System.Drawing.Point(203, 7)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(204, 65)
+        Me.PictureBox1.TabIndex = 9
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AcceptButton = Me.btnSearch
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(424, 258)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.radDate)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtBilling)
+        Me.Controls.Add(Me.txtOutput)
+        Me.Controls.Add(Me.radRADs)
+        Me.Controls.Add(Me.btnSearch)
+        Me.Controls.Add(Me.txtSearch)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Medi-Cal Claim Helper"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtSearch As TextBox
+    Friend WithEvents btnSearch As Button
+    Friend WithEvents radRADs As RadioButton
+    Friend WithEvents txtOutput As RichTextBox
+    Friend WithEvents txtBilling As RichTextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents radDate As RadioButton
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ Medi-Cal RAD codes/Form1.vb
@@ -0,0 +1,63 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Dim radDict As IDictionary(Of String, Tuple(Of String, String)) = New Dictionary(Of String, Tuple(Of String, String))()
+
+    Public Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Using MyReader As New Microsoft.VisualBasic.FileIO.TextFieldParser("RAD_Repository.csv")
+            MyReader.TextFieldType = FileIO.FieldType.Delimited
+            MyReader.SetDelimiters(",")
+            Dim currentRow As String()
+            While Not MyReader.EndOfData
+                currentRow = MyReader.ReadFields
+                If radDict.ContainsKey(currentRow(0)) Then
+                    Continue While
+                Else
+                    Dim info = New Tuple(Of String, String)(currentRow(1), currentRow(2))
+                    radDict.Add(currentRow(0), info)
+                End If
+
+
+            End While
+        End Using
+    End Sub
+
+
+
+    Public Sub searchRad()
+        If radDict.ContainsKey(txtSearch.Text) Then
+            Dim searchValue = radDict(txtSearch.Text)
+            txtOutput.Text = searchValue.Item1
+            txtBilling.Text = searchValue.Item2
+        Else
+            txtOutput.Text = "RAD code not found"
+        End If
+
+
+    End Sub
+
+    Private Sub searchDate()
+        Dim searchInput As Integer
+        searchInput = txtSearch.Text
+        If txtSearch.Text < 366 Then txtOutput.Text = "Dec " + (searchInput - 334).ToString
+        If txtSearch.Text < 335 Then txtOutput.Text = "Nov " + (searchInput - 304).ToString
+        If txtSearch.Text < 305 Then txtOutput.Text = "Oct " + (searchInput - 273).ToString
+        If txtSearch.Text < 274 Then txtOutput.Text = "Sep " + (searchInput - 243).ToString
+        If txtSearch.Text < 244 Then txtOutput.Text = "Aug " + (searchInput - 212).ToString
+        If txtSearch.Text < 213 Then txtOutput.Text = "Jul " + (searchInput - 181).ToString
+        If txtSearch.Text < 182 Then txtOutput.Text = "Jun " + (searchInput - 151).ToString
+        If txtSearch.Text < 152 Then txtOutput.Text = "May " + (searchInput - 120).ToString
+        If txtSearch.Text < 121 Then txtOutput.Text = "Apr " + (searchInput - 90).ToString
+        If txtSearch.Text < 91 Then txtOutput.Text = "Mar " + (searchInput - 59).ToString
+        If txtSearch.Text < 60 Then txtOutput.Text = "Feb " + (searchInput - 31).ToString
+        If txtSearch.Text < 32 Then txtOutput.Text = "Jan " + searchInput.ToString
+
+    End Sub
+    Private Sub btnSearch_Click(sender As Object, e As EventArgs) Handles btnSearch.Click
+        txtOutput.Clear()
+        txtBilling.Clear()
+        If radRADs.Checked Then searchRad()
+        If radDate.Checked Then searchDate()
+    End Sub
+End Class
